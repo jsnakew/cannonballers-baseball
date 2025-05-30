475f6b6956bc472e90e312bee7849c7020431f0d
jsnakew <30990227+jsnakew@users.noreply.github.com>
2025-05-30 11:27:20 -0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "East Valley Cannonballers Baseball";
+export const SITE_DESCRIPTION = "Where winners come to play.";
